43a3f02be3f33f8a51f13b966e5dd7cec9c0ea85
1pedr1n <pedrinfnn@gmail.com>
2024-05-13 00:18:13 -0300

    :fire:

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,6 +1,7 @@
-import { Dimensions, StyleSheet, View, Text } from "react-native";
-const { width } = Dimensions.get("window");
 import React from "react";
+import { Dimensions, StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+
+const { width } = Dimensions.get("window");
 
 export default function TabTwoScreen() {
   return (
@@ -9,10 +10,17 @@ export default function TabTwoScreen() {
         <Text style={styles.headerText}>Perfil</Text>
       </View>
       <View style={styles.profileInfo}>
+        <Image
+          
+          style={styles.profileImage}
+        />
         <Text style={styles.profileText}>Nome: Pedro Morais</Text>
         <Text style={styles.profileText}>E-mail: pedrohs.work@gmail.com</Text>
         <Text style={styles.profileText}>Idade: 18 anos</Text>
         <Text style={styles.profileText}>Localização: São Paulo, Brasil</Text>
+        <TouchableOpacity style={styles.deleteButton}>
+          <Text style={styles.deleteButtonText}>Deletar Conta</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -40,10 +48,28 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     backgroundColor: "#373D45",
     borderRadius: 10,
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
   },
   profileText: {
     fontSize: 18,
     color: "#FFF",
     marginBottom: 10,
+  },
+  deleteButton: {
+    backgroundColor: "#FF6347",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  deleteButtonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
---
+++ interfaces/propsCardMovie.interface.ts
@@ -0,0 +1,5 @@
+import { Movie } from "./movie.interface";
+export interface Props {
+  data: Movie;
+  onPress?: () => void;
+}
