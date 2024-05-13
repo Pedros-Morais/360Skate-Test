854da7a5713f94f4de9ec363e6d8e3fa8036c37f
1pedr1n <pedrinfnn@gmail.com>
2024-05-13 00:26:44 -0300

    :fire:

--- service/api.ts
+++ service/api.ts
@@ -1,10 +1,6 @@
-import axios from "axios";
+
+import { api } from "./configApi";
 const apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTNhYmM5MjViY2VmZTZlYjg0ZGM1MDBhNzgwNWFjNyIsInN1YiI6IjY2NDAxM2Q0NTU5NjAxOWE1ZDdlYWYzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HJr7pEqyibQchznaAHcZh3-5GHdjmHkzAYdWIck1AQE";
-export const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-});
-
-
 export const getMovies = async () => {
   try {
    
